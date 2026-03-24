5de98d3d625e12242fa92b0709fe2c1b8a726dab
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-24 15:34:24 +0900

    chore: 직전 레벨 코드 이전 완료

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/App.kt
@@ -0,0 +1,12 @@
+package woowacourse.kanban
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices.DESKTOP
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, device = DESKTOP)
+fun App() {
+    MaterialTheme {}
+}
---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package woowacourse.kanban.board.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object KanbanCardColor {
+    val SelectedBackground = Color(0xFFE5E7EB)
+    val SelectedContent = Color(0xFF1447E6)
+    val SelectedBorder = Color(0xFF1447E6)
+
+    val DefaultBackground = Color.White
+    val DefaultContent = Color.Black
+    val DefaultBorder = Color(0xFFE5E7EB)
+}
+
+object BoardColor {
+    val TodoHeaderColor = Color(0xFF155DFC)
+    val TodoContentColor = Color(0xFFEFF6FF)
+
+    val InProgressHeaderColor = Color(0xFFE17100)
+    val InProgressContentColor = Color(0xFFFFFBEB)
+
+    val DoneHeaderColor = Color(0xFF00A63E)
+    val DoneContentColor = Color(0xFFF0FDF4)
+}
