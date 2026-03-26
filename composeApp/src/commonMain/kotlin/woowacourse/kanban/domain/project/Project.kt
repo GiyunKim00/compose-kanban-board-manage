e472e5ca53fde7b3a13a82b4e33c6e6744c99e71
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-26 09:56:25 +0900

    form: Project 도메인 주석 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/domain/project/Project.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/domain/project/Project.kt
@@ -14,6 +14,12 @@ class Project(
     val boards: List<Board> = boardList
     val selectedBoard: Board = boardList[selectedBoardIndex]
 
+    /**
+     * BoardList의 Index를 활용하여 프로젝트 탭에서 선택한 보드로 변경합니다.
+     * 추후 보드 생성/수정/삭제 기능 요구사항이 추가될 시 id를 활용하도록 리팩토링할 수 있습니다.
+     * @param newBoardIndex 변경할 보드의 인덱스입니다.
+     * @return 변경된 Project 객체입니다.
+     */
     fun switchBoard(newBoardIndex: Int): Project = Project(
         boardList = boardList,
         selectedBoardIndex = newBoardIndex,
@@ -21,8 +27,13 @@ class Project(
         projectDescription = projectDescription,
     )
 
+    /**
+     * BoardList에 새로운 Board를 추가합니다. Board ID를 활용하여 수정사항이 생긴 Board를 교체합니다.
+     * @param newBoard 추가할 Board 객체입니다.
+     * @return 변경된 Project 객체입니다.
+     */
     fun updateBoard(newBoard: Board): Project = Project(
-        boardList = boardList.map { if (it.title == newBoard.title) newBoard else it },
+        boardList = boardList.map { if (it.boardId == newBoard.boardId) newBoard else it },
         selectedBoardIndex = selectedBoardIndex,
         projectTitle = projectTitle,
         projectDescription = projectDescription,
