228dab3d3273362f2b5a73751db3fcb395f65ebc
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-28 14:26:40 +0900

    fix: 불필요한 Annotation 제거 - CardMangerState 확장함수 / CardTaskState 확장함수의 어노테이션 제거

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/ui/board/common/ManagerStateDisplay.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/ui/board/common/ManagerStateDisplay.kt
@@ -1,10 +1,9 @@
 package woowacourse.kanban.ui.board.common
 
-import androidx.compose.runtime.Composable
 import woowacourse.kanban.domain.card.CardManagerState
 
-@Composable
-fun CardManagerState.toDisplayText() : String {
+
+fun CardManagerState.toDisplayText(): String {
     return when (this) {
         CardManagerState.DINO -> "DINO"
         CardManagerState.FAMES -> "FAMES"
